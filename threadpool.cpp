2ffa84b3caa51df31ebc1345dbe20028ea271bbb
Yusuf Bülbül <electronicengine@users.noreply.github.com>
2020-10-17 11:35:08 +0300

    Add files via upload

--- threadpool.cpp
+++ threadpool.cpp
@@ -17,32 +17,36 @@ void ThreadPool::setWorkerSize(int Size)
 
         std::cout << "Thread " << i << "created" << std::endl;
 
-        while(1)
-        {
+
+            int thread_id = i;
+
             Workers_.emplace_back([=]()
             {
 
-                int thread_id = i;
 
-                std::function<void()> task;
-
+                while(1)
                 {
-                    std::unique_lock<std::mutex> ul(lock);
+                    std::function<void()> task;
 
-                    Condition_.wait(ul, [=]{ return !Tasks_.empty(); });
+                    {
 
-                    task = std::move(Tasks_.front());
-                    Tasks_.pop();
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                        std::unique_lock<std::mutex> ul(lock);
 
+                        Condition_.wait(ul, [=]{ return !Tasks_.empty(); });
+
+                        task = std::move(Tasks_.front());
+                        Tasks_.pop();
+                        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+                    }
+
+                    std::cout << "Thread " << thread_id << " is working" << std::endl;
+
+                    task();
                 }
 
-                std::cout << "Thread " << thread_id << " is working" << std::endl;
-
-                task();
-
             });
-        }
+
     }
 
 }
